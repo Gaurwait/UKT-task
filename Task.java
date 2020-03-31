a4f06932756af97e773f2d960a7350a0f2b55a79
Martina Stankova <martina.stankova@isobar.com>
2020-03-31 20:33:29 +0300

    Adding random code to repository

---
+++ Task.java
@@ -0,0 +1,23 @@
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDAOservice userDAOService;
+
+    @GetMapping("/users")
+    public MappingJacksonValue users() {
+        List<User> users = userDAOService.findAll();
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter(
+                "UserBirthDateFilter", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(users);
+
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
