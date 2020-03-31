9fe1d32f10be1dc66051fdb0f3dc030a7045aa03
Martina Stankova <martina.stankova@isobar.com>
2020-03-31 21:49:14 +0300

    Code change for test-branch2

--- Task.java
+++ Task.java
@@ -25,3 +25,18 @@ public class UserResource {
 
 //test change
 //test change 1
+
+@GetMapping("/users/{id}")
+public EntityModel<User> users(@PathVariable Integer id) {
+    User user = userDAOService.findById(id);
+    if (user == null) {
+        throw new ResourceNotFoundException("id-" + id);
+    }
+
+    EntityModel<User> model = new EntityModel<>(user);
+    WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).users());
+    model.add(linkTo.withRel("all-users"));
+
+    //how do I combine EntityModel with filtering?
+    return model;
+}
